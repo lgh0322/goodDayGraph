f1ec6328a6ce95779c6728cc61f065501fec90a4
李光浩 <liguanghao@viatomtech.com>
2021-08-30 17:56:19 +0800

    二哥dgbdfg

--- app/src/main/java/com/vaca/goodday/MainActivity.kt
+++ app/src/main/java/com/vaca/goodday/MainActivity.kt
@@ -2,10 +2,38 @@ package com.vaca.goodday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import com.vaca.goodday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var vibrator: Vibrator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.up.setOnClickListener {
+            vibrator.vibrate(100)
+        }
+
+
+        binding.down.setOnClickListener {
+            vibrator.vibrate(100)
+        }
+
+
+        binding.left.setOnClickListener {
+            vibrator.vibrate(100)
+
+        }
+
+
+        binding.right.setOnClickListener {
+            vibrator.vibrate(100)
+
+        }
     }
 }
